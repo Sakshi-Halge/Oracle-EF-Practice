ebba94806ddfcc487504c96cb31c276ee802de1b
Sakshi-Halge <sakshihalge2000@gmail.com>
2025-07-17 23:29:32 +0530

    Added context repository

---
+++ Oracle-EF-Practice.Repositories/Repositories/GenericContextRepository.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Oracle_EF_Practice.Repositories.DBContext;
+using Oracle_EF_Practice.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oracle_EF_Practice.Repositories.Repositories
+{
+    public class GenericContextRepository<T> : IGenericContextRepository<T> where T : class
+    {
+        protected readonly AppDbContext _appDbContext;
+
+        public GenericContextRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext ?? throw new ArgumentNullException(nameof(appDbContext));
+        }
+        public Task AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteRangeAsync(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _appDbContext.Set<T>().ToListAsync();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
